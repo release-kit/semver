d3d4e56ffc62d50d7b90bfefbbf308a816a19d1c
Evgeny Zakharov <to@risen.dev>
2023-05-28 07:22:24 +0400

    feat: update to v2

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    GITHUB_REF_NAME: string
+    GITHUB_REF_TYPE: string
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,58 @@
-import { setOutput } from '@actions/core'
-import { extractVersion } from './extract'
+import { info, setFailed, setOutput } from '@actions/core'
+import { parse } from 'semver'
 import { getLatestTag } from './git'
 import { getOptions } from './options'
-import { parseVersion } from './parse'
 
-async function run() {
-  const options = getOptions()
-  let { input } = options
-  const { extract, fallback } = options
+function extractAndSetOutput(string: string, pattern: string) {
+  const regex = new RegExp(pattern, 'g')
+  const matches = regex.exec(string)
 
-  if (!input) {
-    console.info('No input provided, using the latest tag')
-
-    const tag = await getLatestTag({ fallback })
-    input = tag.value
-    setOutput('tag', tag.value)
-
-    if (tag.isFallback) {
-      console.info(`No tag was found, using the fallback: ${fallback}`)
-    }
+  if (matches === null) {
+    return setFailed(`No matches found when using regex: "${pattern}"`)
   }
 
-  const regex = new RegExp(extract, 'g')
-  const version = extractVersion(input, regex)
+  if (matches.length > 2) {
+    return setFailed(`Multiple matches found when using regex: "${pattern}"`)
+  }
 
-  const parsed = parseVersion(version)
+  const [, version] = matches
+
+  const parsed = parse(version)
+
+  if (!parsed) {
+    return setFailed(`Cannot parse "${version}" - most likely it's invalid`)
+  }
+
+  setOutput('full', parsed.raw)
   setOutput('major', parsed.major)
   setOutput('minor', parsed.minor)
   setOutput('patch', parsed.patch)
-  setOutput('prerelease', parsed.prerelease)
-  setOutput('build', parsed.build)
-  setOutput('full', parsed.full)
+  setOutput('prerelease', parsed.prerelease.join('.'))
+  setOutput('build', parsed.build.join('.'))
 }
 
-run()
+async function run() {
+  const options = getOptions()
+
+  if (options.source === 'string') {
+    info(`Using a user's string as an input`)
+    return extractAndSetOutput(options.string, options.pattern)
+  }
+
+  if (options.source === 'workflow-tag') {
+    info(`Using the current tag as an input`)
+
+    if (process.env.GITHUB_REF_TYPE === 'branch') {
+      return setFailed(`No tag found - the workflow is triggered from a branch`)
+    }
+
+    return extractAndSetOutput(process.env.GITHUB_REF_NAME, options.pattern)
+  }
+
+  const { fallback } = options
+  const latestTag = await getLatestTag({ fallback })
+  if (latestTag.isFallback) info(`No tag found - using fallback: "${fallback}"`)
+  return extractAndSetOutput(latestTag.value, options.pattern)
+}
+
+void run()
